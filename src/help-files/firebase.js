c2f5bd95503aecd3b30650b5c25c04179529faab
Dejan Popović <131488966+pop-dejan@users.noreply.github.com>
2024-03-12 19:42:09 +0100

    Update firebase.js

--- src/help-files/firebase.js
+++ src/help-files/firebase.js
@@ -4,15 +4,15 @@ import { getDatabase } from "firebase/database";
 
 export const firebaseConfig = {
   firebase: {
-    apiKey: "AIzaSyBRIVynGu_w7feX7b_bmcvQpdIyu7O9sK0",
-    authDomain: "fantasy-mock-up.firebaseapp.com",
+    apiKey: "**********************************",
+    authDomain: "**********************************",
     databaseURL:
-      "https://fantasy-mock-up-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "fantasy-mock-up",
-    storageBucket: "fantasy-mock-up.appspot.com",
-    messagingSenderId: "574958326647",
-    appId: "1:574958326647:web:4138d321210496be654772",
-    measurementId: "G-DTDK6FD03R",
+      "**********************************",
+    projectId: "**********************************",
+    storageBucket: "**********************************",
+    messagingSenderId: "**********************************",
+    appId: "**********************************",
+    measurementId: "**********************************",
   },
   production: true,
 };
